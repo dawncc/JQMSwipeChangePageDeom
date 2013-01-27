b93a41c040ed9e5bd38971bae38ab8545ed41435
dawncc <dawncc@live.com>
2013-01-27 22:53:42 +0800

    修正滑动翻页
    1、采用JQM的Ajax方式加载页面，并增加预加载功能。
    2、采用$.mobile的接口方法判断自动判断当前页和上下页，无须另外定义规则。
    3、使用ready绑定左右滑动事件，防止事件重复绑定。

--- js/swipePageExtent.js
+++ js/swipePageExtent.js
@@ -17,50 +17,80 @@
      }
 
      //遍历header->navba->a->href的值
-     var pagePathList = [];
-     /*
-     jqm加载页面的方式采用ajax加载模式,加载多个页面后header会有多个
-     */  
-      $(":jqmData(role='header'):first :jqmData(role='navbar') a").each(function(index){
-        pagePathList.push($(this).attr("href"));
-      });
-     var pageCount = pagePathList.length;
-     var currentPage = 0;
+    //  var pagePathList = [];
+    //  /*
+    //  jqm加载页面的方式采用ajax加载模式,加载多个页面后header会有多个
+    //  */  
+    //  $(":jqmData(role='header'):first :jqmData(role='navbar') a").each(function(index){
+    //   pagePathList.push($(this).attr("href"));
+    // });
+    //  var pageCount = pagePathList.length;
+    //  var currentPage = 0;
          // page_id若以#开始，则为page的id， 否则为相对路径需要加载html后缀
-         $(":jqmData(role='page')").each(function() {
+         // $(":jqmData(role='page')").each(function() {
+        //    $(this).on("swiperight", function() {
+        //   // Get the current page number from the id and increment it by 1.
+        //   // 页面文件名和页面ID的前缀必须一样
+        //   var currentPageName = $(this).attr("id") + ".html";
+        //   currentPage = jQuery.inArray(currentPageName, pagePathList);
 
-           $(this).on("swiperight", function() {
-          // Get the current page number from the id and increment it by 1.
-          // 页面文件名和页面ID的前缀必须一样
-          var currentPageName = $(this).attr("id") + ".html";
-          currentPage = jQuery.inArray(currentPageName, pagePathList);
+        //   var nextPage = currentPage +1;
+        //   if (nextPage >= pageCount) {
+        //     nextPage = 0;
+        //   }
+        //   var nextPagePath = pagePathList[nextPage];
+        //   // Transition the page.
+        //   $.mobile.changePage(nextPagePath);
+        //    return false;
+        // });
 
-          var nextPage = currentPage +1;
-          if (nextPage >= pageCount) {
-            nextPage = 0;
-          }
-          var nextPagePath = pagePathList[nextPage];
-          // Transition the page.
-          $.mobile.changePage(nextPagePath);
-           return false;
-        });
+        //    $(this).on("swipeleft", function() {
 
-           $(this).on("swipeleft", function() {
+        //   // Get the current page number from the id and decrement it by 1.
+        //   var currentPageName = $(this).attr("id") + ".html";
+        //   currentPage = jQuery.inArray(currentPageName, pagePathList);
+        //   var prePage = currentPage -1;
+        //   if (prePage < 0) {
+        //     prePage = pageCount -1;
+        //   }
+        //   var prePagePath = pagePathList[prePage];
+        //   $.mobile.changePage(prePagePath);
+        //   return false;
+        // });
+// $(":jqmData(role='page')").each(function() {
+  var page =$("body");
+  var currentPage = $(":jqmData(role='page')");
+  page.on("swiperight", function() {
+    // var nextPage = currentPage.next(":jqmData(role=page)");
+    // if(!nextPage.length)
+    // {
+    //   nextPage = $(":jqmData(role='page'):first");
+    // }
+    // $.mobile.changePage(nextPage);
+    var nextPages = $.mobile.activePage.nextAll('div[data-role=page]');
+    if(nextPages.length !== 0){
+      $.mobile.changePage(nextPages.first());
+    }else{
+     $.mobile.changePage($(":jqmData(role='page'):first"));
+   }
+   
+ });
+  page.on("swipeleft", function() {
+    // var prePage = currentPageName.prev(":jqmData(role=page)");
+    // if(!prePage.length)
+    // {
+    //   prePage = $(":jqmData(role='page'):last");
+    // }
 
-          // Get the current page number from the id and decrement it by 1.
-          var currentPageName = $(this).attr("id") + ".html";
-          currentPage = jQuery.inArray(currentPageName, pagePathList);
-          var prePage = currentPage -1;
-          if (prePage < 0) {
-            prePage = pageCount -1;
-          }
-          var prePagePath = pagePathList[prePage];
-          $.mobile.changePage(prePagePath);
-          return false;
-        });
-
-         })
-
+    // $.mobile.changePage(prePage);
+    var prePages = $.mobile.activePage.prevAll('div[data-role=page]');
+    if(prePages.length !== 0){
+      $.mobile.changePage(prePages.first());
+    }else{
+     $.mobile.changePage($(":jqmData(role='page'):last"));
+   }
+ });
+// });
 }
 }
 
